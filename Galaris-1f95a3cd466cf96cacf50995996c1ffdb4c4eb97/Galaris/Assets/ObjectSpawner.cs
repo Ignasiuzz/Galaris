a19e1d6260c7b0e7c5a910103f4f3d829d17c325
Ghirahim5 <matas.siaurodinas@gmail.com>
2023-12-10 17:20:41 +0200

    FIXED object spawning
    +Added feature: galima prideti ir kitokiu objektu ir nurodyti kokia ju atsiradimo tikimybe

--- Galaris-1f95a3cd466cf96cacf50995996c1ffdb4c4eb97/Galaris/Assets/ObjectSpawner.cs
+++ Galaris-1f95a3cd466cf96cacf50995996c1ffdb4c4eb97/Galaris/Assets/ObjectSpawner.cs
@@ -1,73 +1,150 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public class SpawnableObject
+{
+    public GameObject prefab;
+    public float spawnProbability = 1.0f;
+}
 
 public class ObjectSpawner : MonoBehaviour
 {
-    public GameObject[] objectsToSpawn; // Array of objects to spawn
-    public float spawnDistanceMin = 5f; // Minimum spawn distance from the player
-    public float spawnDistanceMax = 10f; // Maximum spawn distance from the player
-    public float unloadDistance = 20f; // Distance at which objects will be set inactive
-    public float spawnInterval = 5f; // Time interval for spawning objects
+    public int poolSize = 10; // Number of objects in the pool
+    public float spawnDistanceMin = 5f;
+    public float spawnDistanceMax = 10f;
+    public float unloadDistance = 20f;
+    public float spawnInterval = 5f;
+    public int objectsToSpawn = 3; // Number of objects to spawn each time
+    public float minDistanceBetweenObjects = 1f; // Minimum distance between spawned objects
+    public List<SpawnableObject> spawnableObjects = new List<SpawnableObject>(); // List of spawnable prefabs
 
-    [Header("Prefab Settings")]
+    [Header("Player Settings")]
     [SerializeField]
-    private GameObject spawnableObjectPrefab; // Prefab containing sprite and collider
+    private Transform playerTransform; // Player transform
 
-    private Transform player;
+    private List<GameObject> objectPool = new List<GameObject>();
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        InitializeObjectPool();
         StartCoroutine(SpawnObjectsRoutine());
+    }
+
+    void InitializeObjectPool()
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = Instantiate(spawnableObjects[0].prefab, Vector3.zero, Quaternion.identity);
+            obj.SetActive(false);
+            objectPool.Add(obj);
+        }
     }
 
     IEnumerator SpawnObjectsRoutine()
     {
         while (true)
         {
-            SpawnObjects();
-            yield return new WaitForSeconds(spawnInterval); // Adjust the time interval for spawning objects
+            SpawnObjects(objectsToSpawn); // Specify the number of objects to spawn
+            yield return new WaitForSeconds(spawnInterval);
         }
     }
 
-    void SpawnObjects()
+    void SpawnObjects(int numberOfObjects)
     {
-        // Spawn new objects
-        for (int i = 0; i < objectsToSpawn.Length; i++)
+        for (int i = 0; i < numberOfObjects; i++)
         {
-            // Generate random polar coordinates
-            float randomDistance = Random.Range(spawnDistanceMin, spawnDistanceMax);
-            float randomAngle = Random.Range(0f, 360f);
+            for (int j = 0; j < objectPool.Count; j++)
+            {
+                if (!objectPool[j].activeInHierarchy)
+                {
+                    float randomDistance = Random.Range(spawnDistanceMin, spawnDistanceMax);
+                    float randomAngle = Random.Range(0f, 360f);
 
-            // Convert polar coordinates to Cartesian coordinates
-            float spawnX = player.position.x + randomDistance * Mathf.Cos(randomAngle * Mathf.Deg2Rad);
-            float spawnY = player.position.y + randomDistance * Mathf.Sin(randomAngle * Mathf.Deg2Rad);
+                    // Calculate the random position around the player in the x and y plane
+                    Vector3 randomOffset = new Vector3(Mathf.Cos(randomAngle * Mathf.Deg2Rad), Mathf.Sin(randomAngle * Mathf.Deg2Rad)) * randomDistance;
+                    Vector3 randomPosition = playerTransform.position + randomOffset;
 
-            Vector3 randomPosition = new Vector3(spawnX, spawnY, 0f); // Assuming the game is in 2D
+                    // Check if the distance to the player is greater than a minimum proximity
+                    float minProximity = 2f; // Adjust this value based on your preferences
+                    if (Vector3.Distance(randomPosition, playerTransform.position) < minProximity)
+                    {
+                        continue; // Skip this iteration and try again
+                    }
 
-            // Instantiate the prefab directly for each new object
-            GameObject spawnedObject = Instantiate(spawnableObjectPrefab, randomPosition, Quaternion.identity);
+                    // Check if the new object overlaps with existing objects
+                    bool overlap = false;
+                    for (int k = 0; k < objectPool.Count; k++)
+                    {
+                        if (objectPool[k].activeInHierarchy)
+                        {
+                            float distanceBetweenObjects = Vector3.Distance(randomPosition, objectPool[k].transform.position);
 
-            // Set the spawned object to active
-            spawnedObject.SetActive(true);
+                            if (distanceBetweenObjects < minDistanceBetweenObjects)
+                            {
+                                overlap = true;
+                                break;
+                            }
+                        }
+                    }
 
-            // Assign the spawned object to the array
-            objectsToSpawn[i] = spawnedObject;
+                    if (overlap)
+                    {
+                        continue; // Skip this iteration and try again
+                    }
 
-            // Optional: You can perform additional setup on the spawned object if needed
+                    // Randomly select a spawnable prefab
+                    GameObject selectedPrefab = GetRandomPrefab();
+
+                    // Spawn the selected prefab
+                    objectPool[j] = Instantiate(selectedPrefab, randomPosition, Quaternion.identity);
+                    objectPool[j].SetActive(true);
+
+                    // Optional: You can perform additional setup on the spawned object if needed
+                    break; // Break out of the loop after spawning one object
+                }
+            }
         }
+    }
+
+    GameObject GetRandomPrefab()
+    {
+        float totalProbability = 0f;
+
+        // Calculate the total probability of all spawnable objects
+        foreach (var spawnableObject in spawnableObjects)
+        {
+            totalProbability += spawnableObject.spawnProbability;
+        }
+
+        float randomValue = Random.Range(0f, totalProbability);
+        float cumulativeProbability = 0f;
+
+        // Choose the prefab based on the random value and spawn probabilities
+        foreach (var spawnableObject in spawnableObjects)
+        {
+            cumulativeProbability += spawnableObject.spawnProbability;
+
+            if (randomValue <= cumulativeProbability)
+            {
+                return spawnableObject.prefab;
+            }
+        }
+
+        // If for some reason the loop did not return a prefab, return the first one
+        return spawnableObjects[0].prefab;
     }
 
     void Update()
     {
-        for (int i = 0; i < objectsToSpawn.Length; i++)
+        for (int i = 0; i < objectPool.Count; i++)
         {
-            float distanceToPlayer = Vector3.Distance(objectsToSpawn[i].transform.position, player.position);
+            float distanceToPlayer = Vector3.Distance(objectPool[i].transform.position, playerTransform.position);
 
-            // Set the object to inactive if it is beyond the specified distance
             if (distanceToPlayer > unloadDistance)
             {
-                objectsToSpawn[i].SetActive(false);
+                objectPool[i].SetActive(false);
             }
         }
     }
